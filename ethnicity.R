7bc8ea51d7a98595373d3c2188b1968e0a9f1003
Monica Keosombath <mkeosombath@gmail.com>
2017-11-30 12:30:39 -0800

    added a few more graphs, percentage + percentage diff

--- ethnicity.R
+++ ethnicity.R
@@ -1,35 +1,40 @@
-news<- read.csv("EthnicDistribution- New Hires.csv")
-overall <- read.csv("EthnicDistribution - Overall.csv")
-resign <- read.csv("EthnicDistribution - Resignation.csv")
+new.data <- read.csv("data/EthnicDistributionNewHires.csv")
+overall.data <- read.csv("data/EthnicDistributionOverall.csv")
+resign.data <- read.csv("data/EthnicDistributionResignations.csv")
 
 library(ggplot2)
 library(reshape2)
 library(dplyr)
+library(tidyr)
 
 #resignation
-resign <- resign %>%
-  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial) %>%
+resign <- resign.data %>%
+  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial,Not.Reported, Refused.to.Respond,Total) %>%
   arrange(Academic.Year) %>%
   mutate(id = "Resignation")
 
 #overall
-overall <- overall %>%
-  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial) %>%
+overall <- overall.data %>%
+  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial,Not.Reported, Refused.to.Respond,Total) %>%
   arrange(Academic.Year) %>%
   mutate(id = "Overall")
 
 #New hires
-new <- news %>%
-  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial) %>%
+new <- new.data %>%
+  select(Academic.Year, White, Asian, Black, Hispanic, American.Indian, Pacific.Islander, Multi.Racial,Not.Reported, Refused.to.Respond, Total) %>%
   arrange(Academic.Year) %>%
   mutate (id= "New.Hires")
 
 types <- rbind(resign,overall,new)
-all <- melt(types, id.vars = c("Academic.Year", "id"))
+all <- melt(types, id.vars = c("Academic.Year", "id", "Not.Reported", "Refused.to.Respond","Total"))
 
+#calculate percentage change
 all <- all %>%
   group_by(id, variable) %>%
-  mutate(pChange = (value - lag(value))/lag(value) * 100)
+  mutate(pChange.value = (value - lag(value))/lag(value) * 100) %>%
+  replace_na(list(Refused.to.Respond = 0)) %>%
+  mutate(percentage = (value/(Total-(Not.Reported+Refused.to.Respond))*100)) %>%
+  mutate(pDiff = (percentage-lag(percentage)))
 
 #add year (for graphing purposes)
 all <- all %>%
@@ -40,24 +45,38 @@ all$value <- as.numeric(all$value)
 all$Year <- as.numeric(all$Year)
 
 #WHITE
-data_white <- data_white %>%
-  filter(variable == "White") %>%
-  na.omit()
+data_white <- filter(all, variable == "White")
 
-#linear model graph for white faculty
-white.plot <- ggplot(data_white, aes(x = Year, y = pChange, colour = id, group=id))+
+#linear model graph for white faculty PERCENTANGE CHANGE FOR EACH VALUE
+white.plot <- ggplot(data_white, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
-  labs(title = "Percentage Change for White Faculty", x = "Academic Year", y = "Percentage Change (%)")+
+  labs(title = "Percentage Change between each year for White Faculty", x = "Academic Year", y = "Percentage Change (%)")+
+  xlim(2007, 2015)
+
+#percentage of each year
+white.percent <- ggplot(data_white, aes(x = Year, y = percentage, colour = id, group=id))+
+  geom_point()+ xlab("Academic Year") + ylab("Percentage (%)") + 
+  theme_bw() + geom_smooth(method = "lm", se = FALSE)+
+  labs(title = "Percentage for White Faculty each Academic Year", x = "Academic Year", y = "Percentage(%)")+
+  xlim(2007, 2015)
+
+#total percentage DIFFERENCE betweeen each year
+white.percentDiff <- ggplot(data_white, aes(x = Year, y = pDiff, colour = id, group=id))+
+  geom_point()+ xlab("Academic Year") + ylab("Percentage (%)") + 
+  theme_bw() + geom_smooth(method = "lm", se = FALSE)+
+  labs(title = "Percentage Difference for White Faculty", x = "Academic Year", y = "Percentage Diff(%)")+
   xlim(2007, 2015)
 
 white.plot
+white.percent
+white.percentDiff
 
 #ASIAN
 data_asian <- filter(all, variable == "Asian")
 
 #linear model graph for asian faculty
-asian.plot <- ggplot(data_asian, aes(x = Year, y = pChange, colour = id, group=id))+
+asian.plot <- ggplot(data_asian, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for Asian Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -69,7 +88,7 @@ asian.plot
 data_black<- filter(all, variable == "Black")
 
 #linear model graph for black faculty
-black.plot <- ggplot(data_black, aes(x = Year, y = pChange, colour = id, group=id))+
+black.plot <- ggplot(data_black, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for Black Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -81,7 +100,7 @@ black.plot
 data_hispanic<- filter(all, variable == "Hispanic")
 
 #linear model graph for hispanic faculty
-hispanic.plot <- ggplot(data_hispanic, aes(x = Year, y = pChange, colour = id, group=id))+
+hispanic.plot <- ggplot(data_hispanic, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for Hispanic Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -93,7 +112,7 @@ hispanic.plot
 data_indian<- filter(all, variable == "American.Indian")
 
 #linear model graph for hispanic faculty
-indian.plot <- ggplot(data_indian, aes(x = Year, y = pChange, colour = id, group=id))+
+indian.plot <- ggplot(data_indian, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for American Indian Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -104,7 +123,7 @@ indian.plot
 data_islander<- filter(all, variable == "Pacific.Islander")
 
 #linear model graph for hispanic faculty
-islander.plot <- ggplot(data_islander, aes(x = Year, y = pChange, colour = id, group=id))+
+islander.plot <- ggplot(data_islander, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for Pacific Islander Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -116,7 +135,7 @@ islander.plot
 data_multi<- filter(all, variable == "Multi.Racial")
 
 #linear model graph for hispanic faculty
-multi.plot <- ggplot(data_multi, aes(x = Year, y = pChange, colour = id, group=id))+
+multi.plot <- ggplot(data_multi, aes(x = Year, y = pChange.value, colour = id, group=id))+
   geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm", se = FALSE)+
   labs(title = "Percentage Change for Multi Racial Faculty", x = "Academic Year", y = "Percentage Change (%)")+
@@ -129,10 +148,10 @@ multi.plot
 #not related
 #overall percentage change CHANGE for overall chart below
 dat_m <- dat_m %>%
-  mutate(pChange = (value - lag(value))/lag(value) * 100)
+  mutate(pChange.value = (value - lag(value))/lag(value) * 100)
 
 #overall trends between all ethnicities
-g <- ggplot(dat_m, aes(x = Academic.Year, y = pChange, colour = as.factor(variable)))
+g <- ggplot(dat_m, aes(x = Academic.Year, y = pChange.value, colour = as.factor(variable)))
 g + geom_point()+geom_line(aes(group = variable))+theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(title = "Percentage Change of Ethnicity for Resignation", x = "Academic Year", y = "Percentage Change (%)")+ 
   labs(color='Ethnicity') + ylim(-100, 700)
