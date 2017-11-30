50de99ca5c6348a7d695de4b34fdf7b6011918ae
Monica Keosombath <mkeosombath@gmail.com>
2017-11-29 23:30:02 -0800

    less redundant code

--- resignation.R
+++ resignation.R
@@ -13,31 +13,29 @@ ethnicity <- resign %>%
 #melt everything together
 dat_m <- melt(ethnicity, id.vars = "Academic.Year")
 
+#overall percentage change
+dat_m <- dat_m %>%
+  mutate(pChange = (value - lag(value))/lag(value) * 100)
+
+#add year (for graphing purposes)
+dat_m <- mutate(dat_m, Year = substr(dat_m$Academic.Year, 0, 4))
+
 #change type to numeric 
 dat_m$value <- as.numeric(dat_m$value)
+dat_m$Year <- as.numeric(dat_m$Year)
 
-#find percentage change for white faculty
-data_white <- dat_m %>% 
-  filter(variable == "White") %>%
-  group_by(variable) %>%
-  mutate(pChange = (value - lag(value))/lag(value) * 100) %>%
-  mutate(Year = substr(data_white$Academic.Year, 0, 4))
-
-#convert year to numeric value
-data_white$Year = as.numeric(data_white$Year)
+#WHITE
+data_white <- filter(dat_m,variable == "White")
 
 #linear model graph for white faculty
 g <- ggplot(data_white, aes(x = Year, y = pChange, colour))
 g+ geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") + 
   theme_bw() + geom_smooth(method = "lm") +
-  labs(title = "Percentage Change for White Faculty", x = "Academic Year", y = "Percentage Change (%)")
-
-#asian linear model
-data_asian <- dat_m %>% 
-  filter(variable == "Asian") %>%
-  group_by(variable) %>%
-  mutate(pChange = (value - lag(value))/lag(value) * 100) %>%
-  mutate(Year = substr(data_white$Academic.Year, 0, 4))
+  labs(title = "Percentage Change for White Faculty", x = "Academic Year", y = "Percentage Change (%)")+
+  xlim(2007, 2014)
+  
+#ASIAN
+data_asian <- filter(dat_m, variable == "Asian")
 
 #linear model graph for asian faculty
 g <- ggplot(data_asian, aes(x = Year, y = pChange, colour))
@@ -45,8 +43,6 @@ g+ geom_point()+ xlab("Academic Year") + ylab("Percentage Change (%)") +
   theme_bw() + geom_smooth(method = "lm") +
   labs(title = "Percentage Change for Asian Faculty", x = "Academic Year", y = "Percentage Change (%)")+ 
   xlim(2007,2014)
-
-
 
 #overall trends between all ethnicities
 g <- ggplot(dat_m, aes(x = Academic.Year, y = pChange, colour = as.factor(variable)))
